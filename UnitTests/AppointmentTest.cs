21d766fc2baf26b709f580f00568d3ed54a7c149
Kim-Inyo <101404494+Kim-Inyo@users.noreply.github.com>
2022-10-22 23:41:26 +1000

    UnitTests Added

---
+++ UnitTests/AppointmentTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.UseCase;
+using Domain.Models;
+using Domain.Logic;
+using Moq;
+
+namespace UnitTests
+{
+    public class AppointmentTest
+    {
+        private readonly AppointmentService _appointmentService;
+        private readonly Mock<IAppointmentRepository> _appointmentRepositoryMock;
+
+        public AppointmentTest ()
+        {
+            _appointmentRepositoryMock = new Mock<IAppointmentRepository>();
+            _appointmentService = new AppointmentService(_appointmentRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void SaveAppointmentTest()
+        {
+            _appointmentRepositoryMock.Setup(repository => repository.SaveAppointment(
+                It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<int>(), It.IsAny<int>())).Returns(() => null);
+
+            var res = _appointmentService.SaveAppointment(DateTime.MinValue, DateTime.MaxValue, 0, 0);
+            Assert.True(res.IsFailure);
+            Assert.Equal("Failed to Save Appointment", res.Error);
+        }
+
+        [Fact]
+        public void GetFreeTimeTest()
+        {
+            _appointmentRepositoryMock.Setup(repository => repository.GetFreeTime(
+                It.IsAny<Spec>())).Returns(() => null);
+
+            var res = _appointmentService.GetFreeTime(new Spec());
+            Assert.True(res.IsFailure);
+            Assert.Equal("Invalid Spec", res.Error);
+        }
+    }
+}
---
+++ UnitTests/ScheduleTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.UseCase;
+using Domain.Models;
+using Domain.Logic;
+using Moq;
+
+namespace UnitTests
+{
+    public class ScheduleTest
+    {
+        private readonly ScheduleService _scheduleService;
+        private readonly Mock<IScheduleRepository> _scheduleRepositoryMock;
+
+        public ScheduleTest()
+        {
+            _scheduleRepositoryMock = new Mock<IScheduleRepository>();
+            _scheduleService = new ScheduleService(_scheduleRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void GetScheduleOfDoctorTest()
+        {
+            _scheduleRepositoryMock.Setup(repository => repository.GetScheduleOfDoctor(
+                It.IsAny<int>())).Returns(() => null);
+
+            var res = _scheduleService.GetScheduleOfDoctor(0);
+            Assert.True(res.IsFailure);
+            Assert.Equal("Invalid Doctor Id", res.Error);
+            Assert.Equal("Schedule Not Found", res.Error);
+        }
+
+        [Fact]
+        public void AddScheduleTest()
+        {
+            _scheduleRepositoryMock.Setup(repository => repository.AddSchedule(
+                It.IsAny<int>(), It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(() => false);
+
+            var res = _scheduleService.AddSchedule(0, DateTime.MinValue, DateTime.MaxValue);
+            Assert.True(res.IsFailure);
+            Assert.Equal("Invalid Schedule", res.Error);
+        }
+
+        [Fact]
+        public void EditScheduleTest()
+        {
+            _scheduleRepositoryMock.Setup(repository => repository.EditSchedule(
+                It.IsAny<int>(), It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(() => false);
+
+            var res = _scheduleService.EditSchedule(0, DateTime.MinValue, DateTime.MaxValue);
+            Assert.True(res.IsFailure);
+            Assert.Equal("Invalid Schedule", res.Error);
+            Assert.Equal("Invalid Doctor Id", res.Error);
+        }
+    }
+}
